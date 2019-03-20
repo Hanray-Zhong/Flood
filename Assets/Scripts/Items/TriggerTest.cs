746a3d767190ad00d8cd823ee666acf4a8e26311
kaneto <646374316@qq.com>
2019-03-20 17:00:33 +0800

    3.20
    - 移动和触发功能的实现

---
+++ Assets/Scripts/Items/TriggerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour {
+	Color color = new Color(1, 0, 0);
+
+	private void OnTriggerStay2D(Collider2D other) {
+		if (other.tag != "Player") {
+			return;
+		}
+		Debug.Log("get trigger!");
+		if (Input.GetKey(KeyCode.K)) {
+			Debug.Log("get key!");
+			gameObject.GetComponent<SpriteRenderer>().color = color;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private float hl;
+	private float rise;
+	private Vector2 		MoveDir;
+
+	[Header("Move")]
+	public float Speed;
+	public float Rise_speed;
+	public float Drag;
+
+	void FixedUpdate () {
+		HandleConfiguration ();
+		GetMoveDir ();
+		StartMove ();
+		RiseUp();
+	}
+	/// <summary>
+	/// 手柄配置
+	/// </summary>
+	void HandleConfiguration () {
+		hl = Input.GetAxis("Horizontal");
+		rise = Input.GetAxis("Rise");
+	}
+	void GetMoveDir () {
+		MoveDir = new Vector2(0, 0);
+		MoveDir = new Vector2(hl, 0).normalized;
+	}
+	void StartMove () {
+		this.gameObject.GetComponent<Rigidbody2D>().drag = Drag;
+		this.gameObject.GetComponent<Rigidbody2D>().AddForce(MoveDir * Speed * Time.deltaTime);
+	}
+	void RiseUp() {
+		this.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * rise * Rise_speed * Time.deltaTime);
+	}
+}
